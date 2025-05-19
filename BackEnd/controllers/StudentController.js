bf6f58bdf3b896fae2f68f0d1f192db96182c844
MoDemoxy <MoDemoxy@gmail.com>
2025-05-19 09:36:52 +0200

    ✨ Add student sector/languages in API

--- BackEnd/controllers/StudentController.js
+++ BackEnd/controllers/StudentController.js
@@ -1,5 +1,7 @@
+const account_sector = require('../models/account_sector.js');
+const language_student = require('../models/language_student.js');
 const { Model } = require('../models/Model.js');
-const { Student, Account } = Model
+const { Student, Account, LanguageStudent, AccountSector } = Model
 
 exports.create = async (req, res) => {
     try {
@@ -9,13 +11,30 @@ exports.create = async (req, res) => {
             email: req.body.email,
             password: req.body.pass,
             description : "",
+            linkdin: "linkedin",
             name_region : req.body.region ?? "Bretagne"
         });
         const student = await Student.create({
             id_account: account.id,
             surname: req.body.surname,
+            name: req.body.school ?? null,
             disponibility: req.body.disponibility
         })
+        
+        for(const lang of req.body.languages){
+            await LanguageStudent.create({
+                id: student.id,
+                name: lang,
+                natif: false,
+            })
+        }
+        for(const sector of req.body.sector){
+            await AccountSector.create({
+                id: account.id,
+                name: sector,
+            })
+        }
+
         res.status(201).json(student);
     } catch (err) {
         console.error(err);
